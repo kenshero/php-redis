21d9201378c1b173ca72915d50f73d5cc188ad34
kenshero <kenshero.k@gmail.com>
2016-03-12 21:16:24 +0700

    add connect

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+  $serverName = "52.77.236.89";
+  $userName = "xxxx";
+  $userPassword = "xxx";
+  $dbName = "xxx";
+  $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+?>
